f86444955cd5b7870224ecb614015dfd570c12bc
Michael Bush <lil.gordie@gmail.com>
2018-06-15 10:58:43 -0500

    router progress

--- app.js
+++ app.js
@@ -2,8 +2,20 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-const static 
+const bodyParser = require('body-parser');
+const layout = require('./views/layout')
+const PORT = 3000;
+
+
+
 
 app.use(morgan('dev'));
 app.use(express.static('public'));
 
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+});
